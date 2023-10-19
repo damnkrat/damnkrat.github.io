7c2ff46ff9e79016bbda2de28cfb2a0a510aa87c
dannkunt <dannkunt@gmail.com>
2023-10-20 02:57:48 +0300

    I am going to sleep after this commit

--- sdkjs-plugins/content/pyscript_test/scripts/code.py
+++ sdkjs-plugins/content/pyscript_test/scripts/code.py
@@ -2,42 +2,47 @@ import html
 
 import js
 
-document = js.document
-window = js.window
+import pyscript
 
 
 def init():
+    pyscript.Console().print("I am in Init")
+    document.write("Method 1")
+    js.document.write("Method 2")
     messageIE = "This plugin is not supported by IE"
     messageIncognito = "This plugin does not work in the browser's incognito mode. Please switch to normal mode."
-    if "Chrome" in window.navigator.userAgent and "Google Inc" in window.navigator.vendor and not window.AscDesktopEditor:
+    if "Chrome" in pyscript.window.navigator.userAgent and "Google Inc" in pyscript.window.navigator.vendor and not pyscript.window.AscDesktopEditor:
         # check incognito mode only in chrome
         # unfortunately, we can't check for incognito mode in PyScript
+        pyscript.Console().print("I want to run 1")
         showFrame()
-    elif "MSIE" in window.navigator.userAgent or "Trident" in window.navigator.userAgent:
+    elif "MSIE" in pyscript.window.navigator.userAgent or "Trident" in pyscript.window.navigator.userAgent:
+        pyscript.Console().print("I want to run 2")
         createMessage(messageIE)
     else:
+        pyscript.Console().print("I want to run 3")
         showFrame()
 
 
 def createMessage(message):
-    document.getElementById("iframe").style.display = "none"
-    divMessage = document.getElementById("div_message")
+    pyscript.document.getElementById("iframe").style.display = "none"
+    divMessage = pyscript.document.getElementById("div_message")
     divMessage.style.display = "flex"
     divMessage.innerHTML = f"<p id='message' style='text-align:center; font-size:12pt;'>{message}</p>"
     divMessage.style.display = "block"
 
 
 def showFrame():
-    frame = document.getElementById("iframe")
+    frame = pyscript.document.getElementById("iframe")
     frame.src = "https://evgeny-nadymov.github.io/telegram-react/"
     frame.style.display = "block"
 
 
 def button(id):
-    window.Asc.plugin.executeCommand("close", "")
+    pyscript.window.Asc.plugin.executeCommand("close", "")
 
 
 def onTranslate():
-    elem = document.getElementById("message")
+    elem = pyscript.document.getElementById("message")
     if elem:
-        elem.innerHTML = window.Asc.plugin.tr(elem.innerHTML)
+        elem.innerHTML = pyscript.window.Asc.plugin.tr(elem.innerHTML)
